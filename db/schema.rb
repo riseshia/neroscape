edb0fde36c99d3380f2282c4c87447cea35b520b
Shia <rise.shia@gmail.com>
2015-06-14 00:06:02 +0900

    add user profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613085027) do
+ActiveRecord::Schema.define(version: 20150613145948) do
 
   create_table "appearances", force: :cascade do |t|
     t.integer  "creator_id"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20150613085027) do
     t.integer  "level",                  default: 0
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
