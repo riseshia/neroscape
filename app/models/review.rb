4cd0578c68d7a5a72071063b88a3adec5b3ae28a
Shia <rise.shia@gmail.com>
2015-06-27 16:50:48 +0900

    add presense validation to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,12 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
 
+  validates :user_id, presence: true
+  validates :game_id, presence: true
+  validates :score, presence: true
+  validates :content, presence: true
+  validates :reviewed, presence: true
+
   def editable? user
     puts user_id
     return true if user.id == user_id
