f72bc8343f86a1e123485d1a646e26a2095409c9
Shia <rise.shia@gmail.com>
2015-12-26 21:13:49 +0900

    refactoring code on brand_parser

--- lib/getchu/parser/brand_parser.rb
+++ lib/getchu/parser/brand_parser.rb
@@ -2,33 +2,19 @@ module Sinnsi
   # BrandParser
   module BrandParser
     def get_brand_getchu_id(html_piece)
-      brand_piece = get_brand_node html_piece
-      regexp = /search_brand_id=([0-9]+)/
-      if brand_piece
-        regexp.match(brand_piece.css('nobr > a').attr('href').text)[1].strip
-      else
-        parent = find_node_in_table html_piece, 'ブランド：'
-        regexp.match(parent.css('td')[1].css('a').attr('href').text)[1]
-      end
+      parent = find_node_in_table html_piece, 'ブランド：'
+      result = /search_brand_id=([0-9]+)/.match(parent.css('td')[1].css('a').last.attr('href'))
+      result[1].strip if result
     end
 
     def get_brand_name(html_piece)
-      brand_piece = get_brand_node html_piece
-      if brand_piece
-        brand_piece.css('a#brandsite').text.strip
-      else
-        parent = find_node_in_table html_piece, 'ブランド：'
-        parent.css('td')[1].text.delete('（このブランドの作品一覧）').strip
-      end
+      parent = find_node_in_table html_piece, 'ブランド：'
+      parent.css('td')[1].text.delete('（このブランドの作品一覧）').strip
     end
 
     def get_brand_url(html_piece)
       brand_piece = get_brand_node html_piece
-      if brand_piece
-        brand_piece.css('a#brandsite').attr('href').text.strip
-      else
-        ''
-      end
+      brand_piece.css('a#brandsite').attr('href').text.strip if brand_piece
     end
 
     def get_brand_node(html_piece)
