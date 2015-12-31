dd60f100dbc5b20c776878445fe7109141d9492b
Shia <rise.shia@gmail.com>
2015-12-31 17:16:21 +0900

    update db migration

--- db/migrate/20150711041300_create_stacks.rb
+++ db/migrate/20150711041300_create_stacks.rb
@@ -1,8 +1,8 @@
 class CreateStacks < ActiveRecord::Migration
   def change
     create_table :stacks do |t|
-      t.references :game_id, index: true, foreign_key: true
-      t.references :user_id, index: true, foreign_key: true
+      t.references :game, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
--- db/migrate/20151225103634_rename_stack_col_name.rb
+++
@@ -1,6 +0,0 @@
-class RenameStackColName < ActiveRecord::Migration
-  def change
-    rename_column :stacks, :game_id_id, :game_id
-    rename_column :stacks, :user_id_id, :user_id
-  end
-end
